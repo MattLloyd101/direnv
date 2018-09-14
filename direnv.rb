b9342b96d2425fecbdb7ac106dca88d7658c1845
Matthew Lloyd <matlloyd@thoughtworks.com>
2018-09-14 19:03:25 +0100

    direnv-2.16.0

---
+++ direnv.rb
@@ -0,0 +1,22 @@
+class Direnv < Formula
+  desc "Load/unload environment variables based on $PWD"
+  homepage "https://direnv.net/"
+  url "https://github.com/direnv/direnv/archive/v2.16.1.tar.gz"
+  sha256 "a17eb4e3ba7600699541a0ac1d7e820b6faf1f2dcda02f791f6e69548d7bb0aa"
+  head "https://github.com/direnv/direnv.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/direnv/direnv").install buildpath.children
+    cd "src/github.com/direnv/direnv" do
+      system "make", "install", "DESTDIR=#{prefix}"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system bin/"direnv", "status"
+  end
+end
